7a39851f7144ce21e15f03f05942adac6f54faca
SimonWilmotsPXL <simon.wilmots1@gmail.com>
2022-09-13 11:15:11 +0200

    Implemented Speech Recognition to OpenAI API
    Speech recognition goes now straight to OpenAI API

--- JARVIS.py
+++ JARVIS.py
@@ -8,7 +8,7 @@ load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY", default=None)
 
 OPENAI_COMPLETION = openai.Completion()
-MAX_TOKENS = 10
+MAX_TOKENS = 20
 
 # response = openai.Model.retrieve("text-davinci-002")
 
@@ -26,6 +26,25 @@ def answer_question(question):
   )
   return response.choices[0].text.strip()
 
+def listener():
+  recognizer = speech_recognition.Recognizer()
+  with speech_recognition.Microphone() as speech:
+    print("👂 Listening...")
+    recognizer.pause_threshold = 2
+    audio = recognizer.listen(speech)
+    
+    try:
+      print("🔍 Recognizing your ✨Beautiful✨ voice...")
+      query = recognizer.recognize_google(audio, language="en-US")
+      print(f'You said: "{query}" ')
+            
+    except Exception as e:
+      print("❌ I didn't quite get that...")
+      return "I didn't quite get that"
+    return query
+    
 
 if __name__ == '__main__':
-  print(answer_question(input("Question: ")))
+  query = listener()
+  answer = answer_question(query)
+  print(answer)
