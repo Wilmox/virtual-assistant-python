bc2253c9419d15818a302b02b085ccd9a766c25b
SimonWilmotsPXL <simon.wilmots1@gmail.com>
2022-09-13 10:42:09 +0200

    OpenAI API Implementation

--- JARVIS.py
+++ JARVIS.py
@@ -1,15 +1,31 @@
 import os
 import openai
+import speech_recognition
+from dotenv import load_dotenv
+
+load_dotenv()
 
 openai.api_key = os.getenv("OPENAI_API_KEY", default=None)
 
-response = openai.Completion.create(
-  model="text-davinci-002",
-  prompt="I am a highly intelligent question answering bot. If you ask me a question that is rooted in truth, I will give you the answer. If you ask me a question that is nonsense, trickery, or has no clear answer, I will respond with \"Unknown\".\n\nQ: What is human life expectancy in the United States?\nA: Human life expectancy in the United States is 78 years.\n\nQ: Who was president of the United States in 1955?\nA: Dwight D. Eisenhower was president of the United States in 1955.\n\nQ: Which party did he belong to?\nA: He belonged to the Republican Party.\n\nQ: What is the square root of banana?\nA: Unknown\n\nQ: How does a telescope work?\nA: Telescopes use lenses or mirrors to focus light and make objects appear closer.\n\nQ: Where were the 1992 Olympics held?\nA: The 1992 Olympics were held in Barcelona, Spain.\n\nQ: How many squigs are in a bonk?\nA: Unknown\n\nQ: Where is the Valley of Kings?\nA:",
-  temperature=0,
-  max_tokens=100,
-  top_p=1,
-  frequency_penalty=0.0,
-  presence_penalty=0.0,
-  stop=["\n"]
-) 
+OPENAI_COMPLETION = openai.Completion()
+MAX_TOKENS = 10
+
+# response = openai.Model.retrieve("text-davinci-002")
+
+def answer_question(question):
+  prompt = f"You: {question}\n J.A.R.V.I.S.: "
+  response = OPENAI_COMPLETION.create(
+    model="text-davinci-002",
+    prompt=prompt,
+    temperature=0.5,
+    max_tokens=MAX_TOKENS,
+    top_p=1,
+    frequency_penalty=0.0,
+    presence_penalty=0.0,
+    stop=["You"]
+  )
+  return response.choices[0].text.strip()
+
+
+if __name__ == '__main__':
+  print(answer_question(input("Question: ")))
